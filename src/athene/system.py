a3b8c62765272da0f5b8d490e29b46eb0178d156
James Thorne <james@jthorne.co.uk>
2019-04-27 11:01:29 +0100

    assign embeddings

--- src/athene/system.py
+++ src/athene/system.py
@@ -93,6 +93,13 @@ def fever_app(caller):
     vocab, embeddings = load_whole_glove(Config.glove_path)
     vocab = vocab_map(vocab)
 
+    # RTE
+    logger.info("Setup RTE")
+    rte_predictor = get_estimator(Config.estimator_name, Config.ckpt_folder)
+    rte_predictor.embedding = embeddings
+
+    logger.info("Restore RTE Model")
+    rte_predictor.restore_model(rte_predictor.ckpt_path)
 
     # Document Retrieval
     logger.info("Setup document retrieval")
@@ -126,12 +133,6 @@ def fever_app(caller):
     logger.info("Load FastText")
     fasttext_model = FastText.load_fasttext_format(Config.fasttext_path)
 
-    # RTE
-    logger.info("Setup RTE")
-    rte_predictor = get_estimator(Config.estimator_name, Config.ckpt_folder)
-
-    logger.info("Restore RTE Model")
-    rte_predictor.restore_model(rte_predictor.ckpt_path)
 
 
     def get_docs_line(line):
